1993cce26983e27d5b6e6a037a57b4117e3f5d3b
Carlotta-2024 <carlotta.westphalen@outlook.com>
2024-12-12 18:00:28 +0100

    Create Recipe Form
    Functionality klappt noch nicht

--- Backend/CourseAdminSystem.Model/obj/Debug/net8.0/CourseAdminSystem.Model.AssemblyInfo.cs
+++ Backend/CourseAdminSystem.Model/obj/Debug/net8.0/CourseAdminSystem.Model.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CourseAdminSystem.Model")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99b077a90addaea1166c6efbb3ac32404816da8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbd0ddefe41a73e8f82affad945dfd62ebf3e663")]
 [assembly: System.Reflection.AssemblyProductAttribute("CourseAdminSystem.Model")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CourseAdminSystem.Model")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
