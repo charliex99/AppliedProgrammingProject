b8ba934fdbe319ee86d32877d6885bdbe03cc853
charliex99 <charlotte.schroeder@hotmail.de>
2024-12-23 09:22:41 +0100

    UX changes

--- CourseAdminSystemAngular/src/app/user/user.component.ts
+++ CourseAdminSystemAngular/src/app/user/user.component.ts
@@ -3,13 +3,15 @@ import { Component, Input } from '@angular/core';
 import { User } from '../model/user';
 import { UserService } from '../services/user.service';
 import { Router } from '@angular/router';
+import {MatCardModule} from '@angular/material/card';
+import {FormsModule} from '@angular/forms';
 
 
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, FormsModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 
